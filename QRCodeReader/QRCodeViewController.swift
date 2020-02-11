3c8e203cebed6dea9343566af4fe7ff50d725109
laikikado <paul.colombier@gmail.com>
2020-02-11 09:19:51 +0100

    Migration du projet en Swift - iOS

---
+++ QRCodeReader/QRCodeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  QRCodeViewController.swift
+//  QRCodeReader
+//
+//  Created by Paul Colombier on 05/02/2020.
+//  Copyright © 2020 Paul Colombier. All rights reserved.
+//
+
+import UIKit
+
+class QRCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // Navigation
+
+    @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
