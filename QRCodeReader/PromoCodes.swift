296d9469a005f53da00cefa353d71e44da2251cc
laikikado <paul.colombier@gmail.com>
2020-02-12 15:17:06 +0100

    Ajout de l'écran des Codes Promos

---
+++ QRCodeReader/PromoCodes.swift
@@ -0,0 +1,28 @@
+//
+//  PromoCodes.swift
+//  QRCodeReader
+//
+//  Created by Paul Colombier on 12/02/2020.
+//  Copyright © 2020 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class PromoCodes: UIViewController {
+
+    @IBAction func Back(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
